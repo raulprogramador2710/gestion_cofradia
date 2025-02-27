050948c98d260475b1a37a032fddb4225f456711
raulprogramador2710 <raulpogramador2710@gmail.com>
2025-02-27 18:10:32 +0100

    Añadir archivo wsgi.py

--- gestion_cofradia/wsgi.py
+++ gestion_cofradia/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Aquí se especifica el archivo de configuración de Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_cofradia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cofradia.settings')
 
 # Se crea la aplicación WSGI que será usada por Gunicorn
 application = get_wsgi_application()
