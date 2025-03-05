8455e0528645f8b85707e971e523299cea5e7c7c
raulprogramador2710 <raulpogramador2710@gmail.com>
2025-03-05 23:29:47 +0100

    1.0.0.3

--- cofradia/settings.py
+++ cofradia/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app.gestion_cofradia.middleware.CurrentUserMiddleware',
+    'apps.gestion_cofradia.middleware.CurrentUserMiddleware',
 ]
 
 # Esto es para manejar archivos estáticos en producción
