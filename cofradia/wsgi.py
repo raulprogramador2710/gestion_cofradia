c37e0ff81336c0fffff5d84be35460fa7be7056e
raulprogramador2710 <raulpogramador2710@gmail.com>
2025-03-06 01:55:23 +0100

    1.0.1.3

--- cofradia/wsgi.py
+++ cofradia/wsgi.py
@@ -3,6 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cofradia.settings')
+os.system("python manage.py migrate gestion_cofrade zero --fake")
 os.system("python manage.py migrate --noinput")
 
 application = get_wsgi_application()
