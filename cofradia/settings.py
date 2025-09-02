46e1d9ffb4a8bd02fd9124a112fecd5d7fd103f6
raulprogramador2710 <raulpogramador2710@gmail.com>
2025-09-02 21:24:05 +0200

    añadir gestor de imagenes nube

--- cofradia/settings.py
+++ cofradia/settings.py
@@ -152,3 +152,18 @@ LOGGING = {
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+
+#keyID: 00326f45d58abda0000000001
+#keyName: DjangoCofradiaKey
+#applicationKey: K003YNfrRRGODaFhVjiiSFqcf3yQXbc
+#S3 Endpoint: https://s3.us-west-002.backblazeb2.com
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
+AWS_QUERYSTRING_AUTH = False   # Así las imágenes tienen URL pública
+MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/"
